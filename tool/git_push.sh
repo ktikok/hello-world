b890d1bddcd7adf5535e60743ae6475a8ceb6c8b
tikim <xhdxhd6226@gmail.com>
2020-08-21 18:05:01 +0900

    git model by model

--- tool/git_push.sh
+++ tool/git_push.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 #default defaultnorm1 defaultnorm0
 #"sgd adam radam ranger"
-for j in `echo "defaultnorm0 defaultnorm1"`
+#run__lr_1e-3__batch_64__optimizer_radam/
+for j in `echo "defaultnorm1"`
   do
-    for i in `echo "radam"`
+    for i in `echo "sgd adam radam ranger"`
       do
 
-        SAMPLEDIR=/store/hep/users/jhgoh/nurion4hep/20200808_2/hdf5_32PU_224x224/ python train_labelByUser.py --epoch 50 --batch 64 --lr 1e-3 --optimizer $i --device 1 --model $j -o ~/work/result/32PU_trackpt/"$j"_"$i"/
+        mkdir /u/user/xhdxhd6226/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
+        cp /u/user/xhdxhd6226/work/result/32PU_trackpt/"$j"_"$i"/*.csv /u/user/xhdxhd6226/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
+        gzip /u/user/xhdxhd6226/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/*
+        cp /u/user/xhdxhd6226/work/result/32PU_trackpt/"$j"_"$i"/*.txt /u/user/xhdxhd6226/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
+        git add .
+        git commit -m "new results"
+        git push
+
 
       done
   done

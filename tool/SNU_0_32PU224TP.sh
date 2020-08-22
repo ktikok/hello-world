203b1c8ac12c6524a71701894159f4771d8ec8db
tikim <49721015+ktikok@users.noreply.github.com>
2020-08-22 14:19:48 +0900

    bash, server by server

---
+++ tool/SNU_0_32PU224TP.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#default defaultnorm1 defaultnorm0
+#"sgd adam radam ranger"
+for j in `echo "default"`
+  do
+    for i in `echo "sgd adam"`
+      do
+
+        python train_labelByUser.py --model "$j" --lr 1e-3 --epoch 50 --batch 64 -o ~/work/result/20200822/run__model_"$j"__lr_1e-3__trackCh_pt__batch_64__optimizer_"$i" --optimizer "$i" --device 0 -c config.yaml
+      done
+  done

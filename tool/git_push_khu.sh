db0157eefd4b6ca153ef79a02acf2f81b99b6770
tikim <49721015+ktikok@users.noreply.github.com>
2020-08-25 01:53:53 +0900

    --lr 1e-3 --epoch 50 --batch 64 --device 2 -c config.yaml

--- tool/git_push_khu.sh
+++ tool/git_push_khu.sh
@@ -6,7 +6,7 @@
 
 #run__lr_1e-3__batch_64__optimizer_radam/
 
-k="count"
+k="pt"
 for j in `echo "default defaultnorm1 defaultnorm0"`
   do
     for i in `echo "sgd adam radam ranger"`
@@ -17,11 +17,13 @@ for j in `echo "default defaultnorm1 defaultnorm0"`
         #gzip /u/user/xhdxhd6226/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/*
         #cp /u/user/xhdxhd6226/work/result/32PU_trackpt/"$j"_"$i"/*.txt /u/user/xhdxhd6226/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
         mkdir ~/work/HEP-CNN-results/training/224x224_track"k"/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
-        cp ~/work/result/32PU_trackcount/"$j"_"$i"/*.csv
+        cp ~/work/result/20200822/run__model_"$j"*optimizer_"$i"/*.csv ~/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
+        gzip ~/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/*
+        cp ~/work/result/20200822/run__model_"$j"*optimizer_"$i"/*.txt ~/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
         
       done
   done
 
 git add .
-git commit -m "new results"
+git commit -m " --lr 1e-3 --epoch 50 --batch 64 --device 2 -c config.yaml"
 git push

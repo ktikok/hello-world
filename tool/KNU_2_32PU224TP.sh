b289965bf90c98665efc235c5a2ee1df776b1d9b
tikim <49721015+ktikok@users.noreply.github.com>
2020-08-26 15:21:58 +0900

    name test

--- tool/KNU_2_32PU224TP.sh
+++ tool/KNU_2_32PU224TP.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #default defaultnorm1 defaultnorm0
 #"sgd adam radam ranger"
-for k in `echo "64 128"`
+for k in `echo "256 512"`
   do
   for j in `echo "defaultnorm0"`
     do
       for i in `echo "adam"`
         do
   
-          python train_labelByUser.py --model "$j" --lr 1e-3 --epoch 50 --batch 64 -o ~/work/result/20200825/run__model_"$j"__lr_1e-3__trackCh_pt__batch_64__optimizer_"$i"__pbs_"$k" --optimizer "$i" --device 1 --batchPerStep "$k" -c config.yaml
+          python train_labelByUser.py --model "$j" --lr 1e-3 --epoch 50 --batch 64 -o ~/work/result/20200825/run__model_"$j"__lr_1e-3__trackCh_pt__batch_64__optimizer_"$i"__pbs_"$k" --optimizer "$i" --device 2 --batchPerStep "$k" -c config.yaml
         done
     done
   done

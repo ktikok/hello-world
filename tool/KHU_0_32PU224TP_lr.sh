01a5f3507c355642fe07c47bf2532805b877a1ed
tikim <49721015+ktikok@users.noreply.github.com>
2020-08-26 20:34:06 +0900

    regular update

---
+++ tool/KHU_0_32PU224TP_lr.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#default defaultnorm1 defaultnorm0
+#"sgd adam radam ranger"
+# 64 128 256 512
+for k in `echo "512"`
+  do
+  for j in `echo "defaultnorm0"`
+    do
+    for i in `echo "adam"`
+      do
+      for l in `echo "5e-4 1e-3 2e-3 5e-3"`
+        do
+        source /store/hep/common/sw/anaconda3/etc/profile.d/conda.sh; conda activate pytorch150_pyG; python train_labelByUser.py  --optimizer "$i" --model "$j" --lr "$l" --epoch 50 --batch 64 --batchPerStep "$k"  --device 0 -o ~/work/result/20200826/"$j"__"$i"__batch_64__pbs_"$k"__lr_"$l" -c config.yaml
+        done  
+      done
+    done
+  done
+#python train_labelByUser.py --model "$j" --lr 1e-3 --epoch 50 --batch 64 -o ~/work/result/20200822/run__model_"$j"__lr_1e-3__trackCh_pt__batch_64__optimizer_"$i" --device 0 --optimizer "$i" -c config.yaml
--- tool/git_push_khu.sh
+++ tool/git_push_khu.sh
@@ -16,14 +16,15 @@ for j in `echo "default defaultnorm1 defaultnorm0"`
         #cp /u/user/xhdxhd6226/work/result/32PU_trackpt/"$j"_"$i"/*.csv /u/user/xhdxhd6226/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
         #gzip /u/user/xhdxhd6226/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/*
         #cp /u/user/xhdxhd6226/work/result/32PU_trackpt/"$j"_"$i"/*.txt /u/user/xhdxhd6226/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
-        mkdir ~/work/HEP-CNN-results/training/224x224_track"k"/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
+        rm -r ~/work/HEP-CNN-results/training/224x224_track"$k"/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
+        mkdir ~/work/HEP-CNN-results/training/224x224_track"$k"/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
         cp ~/work/result/20200822/run__model_"$j"*optimizer_"$i"/*.csv ~/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
-        gzip ~/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/*
+        gzip ~/work/HEP-CNN-results/training/224x224_track"$k"/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/*
         cp ~/work/result/20200822/run__model_"$j"*optimizer_"$i"/*.txt ~/work/HEP-CNN-results/training/224x224_trackpt/model_"$j"/run__lr_1e-3__batch_64__optimizer_"$i"/
         
       done
   done
 
-git add .
-git commit -m " --lr 1e-3 --epoch 50 --batch 64 --device 2 -c config.yaml"
-git push
+#git add .
+#git commit -m "tikim, results of model and optimizer scan"
+#git push

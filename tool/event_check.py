da7c2658f743f7c932bcd798f92c5888b465574c
tikim <49721015+ktikok@users.noreply.github.com>
2020-09-10 14:40:40 +0900

    khu events

--- tool/event_check.py
+++ tool/event_check.py
@@ -1,11 +1,16 @@
 import h5py
 
-for i in range(1,12):
-	d=h5py.File('/xrootd/store/user/jiwoong/HEP_CNN_Large_Image/hdf5_32PU_224x224/QCDBkg/HT1000to1500/data_'+str(i)+'.h5', 'r')
+for i in range(1,13):
+	#d=h5py.File('/xrootd/store/user/jiwoong/HEP_CNN_Large_Image/hdf5_32PU_224x224/QCDBkg/HT1000to1500/data_'+str(i)+'.h5', 'r')
 	#d=h5py.File('/xrootd/store/user/hnam/sample4Nurion/2018DAS/Delphes/presel/hdf5_32PU_224x224/RPV/Gluino1400GeV/0000/RPV_1400-'+str(i)+'.h5', 'r')
-  	
-	print('noPU_224_train_'+str(i)+'.h5')
-	print('images ',len(d['all_events']['images']),'labels ',len(d['all_events']['labels']),'weights ',len(d['all_events']['weights']))
+  # ui20
+
+  d=h5py.File('/store/local1/users/jhgoh/nurion4hep/20200808_2/hdf5_32PU_224x224/QCDBkg/HT1000to1500/data_'+str(i)+'.h5', 'r')
+	# khu
+  
+  print('data_'+str(i)+'.h5')
+  print('images ',len(d['all_events']['images']),'labels ',len(d['all_events']['labels']),'weights ',len(d['all_events']['weights']))
+
 #print(str(d.keys()))
 #if (
 #for i in str(d.keys):

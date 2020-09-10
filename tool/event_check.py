333f1ce498f70c81b0a62fda4bdb631a51f5d0f3
TONGIL KIM <tikim@ui20.sdfarm.kr>
2020-09-10 14:31:02 +0900

    event check ui20

--- tool/event_check.py
+++ tool/event_check.py
@@ -1,10 +1,11 @@
 import h5py
 
-for i in range(1,33):
-  #d=h5py.File('/users/tongilkim/work/data/noPU_224/noPU_224_train_'+str(i)+'.h5', 'r')
-  d=h5py.File('/users/tongilkim/work/data/noPU_224/noPU_224_train_'+str(i)+'.h5', 'r')
-  print('noPU_224_train_'+str(i)+'.h5')
-  print('images ',len(d['all_events']['images']),'labels ',len(d['all_events']['labels']),'weights ',len(d['all_events']['weights']))
+for i in range(1,12):
+	d=h5py.File('/xrootd/store/user/jiwoong/HEP_CNN_Large_Image/hdf5_32PU_224x224/QCDBkg/HT1000to1500/data_'+str(i)+'.h5', 'r')
+	#d=h5py.File('/xrootd/store/user/hnam/sample4Nurion/2018DAS/Delphes/presel/hdf5_32PU_224x224/RPV/Gluino1400GeV/0000/RPV_1400-'+str(i)+'.h5', 'r')
+  	
+	print('noPU_224_train_'+str(i)+'.h5')
+	print('images ',len(d['all_events']['images']),'labels ',len(d['all_events']['labels']),'weights ',len(d['all_events']['weights']))
 #print(str(d.keys()))
 #if (
 #for i in str(d.keys):
